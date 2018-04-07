9b2e137dfeab42c427e7127fa5c779180e2f9e8a
John Emrick <emrickj248@comcast.net>
2018-04-07 00:40:46 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+   ini_set('display_errors', 'On');
+   error_reporting(E_ALL);
+
+// Load XML file
+$xml = new DOMDocument;
+$xml->load('data/website.xml');
+
+// Load XSL file
+$xsl = new DOMDocument;
+$xsl->load('xslt/theme3.xsl');
+
+// Configure the transformer
+$proc = new XSLTProcessor;
+
+// Attach the xsl rules
+$proc->importStyleSheet($xsl);
+
+echo $proc->transformToXML($xml);
+?>
