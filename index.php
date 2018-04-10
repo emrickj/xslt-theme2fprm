07a631919bfacb18586e4ddec0c238a0414f0e59
John Emrick <emrickj248@comcast.net>
2018-04-09 23:50:47 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
-// Bootstrap Scrollspy XSLT theme.
+// Bootstrap 4 Navigation Bar, fixed-top, blue XSLT theme.
 
 ini_set('display_errors', 'On');
 error_reporting(E_ALL);
+
+if(isset($_GET['p'])) $p = $_GET['p'];
+else $p="1";
 
 // Load XML file
 $xml = new DOMDocument;
@@ -10,10 +13,11 @@ $xml->load('data/website.xml');
 
 // Load XSL file
 $xsl = new DOMDocument;
-$xsl->load('xslt/theme3.xsl');
+$xsl->load('xslt/theme2fprm.xsl');
 
 // Configure the transformer
 $proc = new XSLTProcessor;
+$proc->setParameter('', 'page', intval($p));
 
 // Attach the xsl rules
 $proc->importStyleSheet($xsl);
